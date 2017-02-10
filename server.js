c1a66afdb6677891ded6b9eea295dd39edca605d
marimuthur <ezhilmukil@gmail.com>
2017-02-10 20:07:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/rose.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'rose.png'));
+app.get('/ui/rose.PNG', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'rose.PNG'));
 });
 
 
