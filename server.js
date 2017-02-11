fd502a7831d120cd35551c9b284397314b34ac58
marimuthur <ezhilmukil@gmail.com>
2017-02-11 15:26:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/ui/sasipphoto06feb2017b.JPG', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'imad.jpeg'));
+  res.sendFile(path.join(__dirname, 'ui', 'sasipphoto06feb2017b.JPG'));
 });
 
 
