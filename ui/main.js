3d45811c937a5d0bd96332d32d29366b8dd48171
marimuthur <ezhilmukil@gmail.com>
2017-02-15 15:55:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,24 @@
-app.get('/counter',function(req,res){
-        counter=counter+1;
-        res.send(counter,tostring());
-        });
+        //counter code
+var counter=0;
+var button = document.getElementById("counter");
+button.onclick=function(){
+    
+    //create a XML req
+       var request= new XMLHttpRequest();
+    
+    //capture the response and store it in a variable
+       request.onreadystatechange = function() {
+           
+         if(request.readyState===XMLHttpRequest.DONE) {
+             if(request.status===200){
+                 var counter=request.responseText;
+                 var span = document.getElementById("count");
+                 span.innerHTML = counter.toString();
+             }
+         }  
+       };
+    
+    //Making the request
+       request.open('GET','http://marimuthur.imad.hasura-app.io/counter',true);
+       request.send(null);
+};
