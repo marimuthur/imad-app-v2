b894cc046ea780c62c9d45bce77f09980552ba44
marimuthur <ezhilmukil@gmail.com>
2017-02-15 15:47:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -23,6 +23,9 @@ app.get('/ui/vaishu.png', function (req, res) {
 app.get('/ui/akilafamily.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'akilafamily120217.png'));
 });
+app.get('/ui/dad.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'dad.png'));
+});
 app.get('/counter',function(req,res){
         counter=counter+1;
         res.send(counter,tostring());
